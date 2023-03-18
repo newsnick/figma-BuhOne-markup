2b5d5a95aa773c83a48d97708e6ddf5aaf61ac88
Antonio Lega <atlcontact8@gmail.com>
2023-03-18 23:58:15 +0100

    new: random BG-color on each img click/ $variables

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,109 +1,113 @@
 //////////////////////Fetch post
 
-const cubes = document.querySelectorAll(".cube");
-const postsContainer = document.querySelector(".posts");
+const cubes = document.querySelectorAll('.cube')
+const postsContainer = document.querySelector('.posts')
 
 function getRandomPostId() {
-  return Math.floor(Math.random() * 100) + 1;
+  return Math.floor(Math.random() * 100) + 1
 }
 
 function fetchPost() {
-  const postId = getRandomPostId();
-  const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+  const postId = getRandomPostId()
+  const url = `https://jsonplaceholder.typicode.com/posts/${postId}`
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const post = document.createElement("div");
-      post.classList.add("post");
+      const post = document.createElement('div')
+      post.classList.add('post')
       post.innerHTML = `
         <h2>${data.title}</h2>
         <p>${data.body}</p>
-      `;
-      postsContainer.innerHTML = "";
-      postsContainer.appendChild(post);
+      `
+      postsContainer.innerHTML = ''
+      postsContainer.appendChild(post)
     })
     .catch((error) => {
-      console.error("Error fetching post:", error);
-    });
+      console.error('Error fetching post:', error)
+    })
 }
 
 cubes.forEach((cube) => {
-  cube.addEventListener("click", fetchPost);
-});
+  cube.addEventListener('click', fetchPost)
+})
 /////////////////////////////
 ////XHR///
 
-const testimonialsContainer = document.querySelector(".my-testimonials");
-const leftArrow = document.querySelector(".left-arrow");
-const rightArrow = document.querySelector(".right-arrow");
+const testimonialsContainer = document.querySelector('.my-testimonials')
+const leftArrow = document.querySelector('.left-arrow')
+const rightArrow = document.querySelector('.right-arrow')
 
-let currentTestimonialIndex = 0;
+let currentTestimonialIndex = 0
 
 const fetchTestimonials = () => {
   return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts')
     xhr.onload = () => {
       if (xhr.status === 200) {
-        resolve(JSON.parse(xhr.responseText));
+        resolve(JSON.parse(xhr.responseText))
       } else {
-        reject(new Error("Network response was not ok"));
+        reject(new Error('Network response was not ok'))
       }
-    };
+    }
     xhr.onerror = () => {
-      reject(new Error("There was an error with the request"));
-    };
-    xhr.send();
+      reject(new Error('There was an error with the request'))
+    }
+    xhr.send()
   })
     .then((testimonials) => {
-      renderTestimonial(testimonials[currentTestimonialIndex]);
+      renderTestimonial(testimonials[currentTestimonialIndex])
 
-      return testimonials;
+      return testimonials
     })
     .catch((error) => {
-      console.error("Error fetching testimonials:", error);
-    });
-};
+      console.error('Error fetching testimonials:', error)
+    })
+}
 
 const renderTestimonial = (testimonial) => {
-  testimonialsContainer.textContent = testimonial.body;
+  testimonialsContainer.textContent = testimonial.body
 
-  const profilePicture = document.createElement("img");
+  const profilePicture = document.createElement('img')
   profilePicture.src = `https://picsum.photos/50?random=${Math.floor(
     Math.random() * 100
-  )}`;
-  testimonialsContainer.appendChild(profilePicture);
-};
+  )}`
+  testimonialsContainer.appendChild(profilePicture)
+}
 
 const showPreviousTestimonial = () => {
   if (currentTestimonialIndex > 0) {
-    currentTestimonialIndex--;
-    renderTestimonial(testimonials[currentTestimonialIndex]);
+    currentTestimonialIndex--
+    renderTestimonial(testimonials[currentTestimonialIndex])
   }
-};
+}
 
 const showNextTestimonial = () => {
   if (currentTestimonialIndex < testimonials.length - 1) {
-    currentTestimonialIndex++;
-    renderTestimonial(testimonials[currentTestimonialIndex]);
+    currentTestimonialIndex++
+    renderTestimonial(testimonials[currentTestimonialIndex])
   }
-};
+}
 
 fetchTestimonials().then((fetchedTestimonials) => {
-  testimonials = fetchedTestimonials;
-  leftArrow.addEventListener("click", showPreviousTestimonial);
-  rightArrow.addEventListener("click", showNextTestimonial);
-});
+  testimonials = fetchedTestimonials
+  leftArrow.addEventListener('click', showPreviousTestimonial)
+  rightArrow.addEventListener('click', showNextTestimonial)
+})
 
 ////////////////////change background on img click
-const images = document.querySelectorAll(".images_container2 img");
+
+const images = document.querySelectorAll('.images_container2 img')
 
 images.forEach((image) => {
-  image.addEventListener("click", () => {
-    document.body.style.backgroundColor = "#000";
-    document.body.style.color = "#fff";
-    document.p.style.color = "#fff";
-    document.getElementById("container2").style.backgroundColor = "#000";
-  });
-});
+  image.addEventListener('click', () => {
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`
+    document.body.style.backgroundColor = randomColor
+    document.body.style.color = '#fff'
+    document.querySelector('p').style.color = '#fff'
+    document.getElementById('container2').style.backgroundColor = randomColor
+  })
+})
